0542d953421986a0b43123328827c8e6a275d942
patrick <patrick_dj@163.com>
2012-04-06 17:50:26 +0800

    .

--- src/actor.c
+++ src/actor.c
@@ -5,7 +5,7 @@
 MZ_API mz_actor_t* mz_actor_new(const char *file, size_t size, mz_node_t *parent)
 {
     mz_actor_t *v = (mz_actor_t*)mz_node_new(size, parent);
-    mz_texture_load(file);
+    //mz_texture_load(file);
 
     return v;
 }
--- src/core/graphics.c
+++ src/core/graphics.c
@@ -6,28 +6,28 @@ GLuint texture;
 
 void clear()
 {
-	glClearColor(1, 1, 1, 1);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(1, 1, 1, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 void flush()
 {
-	SDL_GL_SwapBuffers();
+    SDL_GL_SwapBuffers();
 }
 
 void draw_texture(TEXTURE_ID id)
 {
-	glLoadIdentity();
+    glLoadIdentity();
 
-	glBindTexture(GL_TEXTURE_2D, texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
 
-	glEnable (GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable (GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	glBegin(GL_QUADS);
-		glTexCoord2i(0, 0); glVertex3i(0, 0, 0);
-		glTexCoord2i(1, 0); glVertex3i(256, 0, 0);
-		glTexCoord2i(1, 1); glVertex3i(256, 256, 0);
-		glTexCoord2i(0, 1); glVertex3i(0, 256, 0);
-	glEnd();
+    glBegin(GL_QUADS);
+        glTexCoord2i(0, 0); glVertex3i(0, 0, 0);
+        glTexCoord2i(1, 0); glVertex3i(256, 0, 0);
+        glTexCoord2i(1, 1); glVertex3i(256, 256, 0);
+        glTexCoord2i(0, 1); glVertex3i(0, 256, 0);
+    glEnd();
 }
