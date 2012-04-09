a6c30792f920863b385b7f14ad0a4e4241d7bf99
patrick <patrick_dj@163.com>
2012-04-09 13:50:58 +0800

    .

---
+++ premake4_makefile.lua
@@ -0,0 +1,28 @@
+if not _PREMAKE_VERSION then
+   os.execute('premake4 --file=premake4_makefile.lua clean')
+   os.execute('premake4 --file=premake4_makefile.lua gmake')
+   return
+end
+
+
+solution "libmz"
+   configurations { "Debug", "Release" }
+ 
+   -- A project defines one build target
+   project "libmz"
+      kind "ConsoleApp"
+      language "C"
+      location ( "build" )
+      targetdir ( "bin" )
+      libdirs { "/usr/lib" }
+      links { "SDL", "SDL_image", "GLEW", "GL" }
+      includedirs { "/usr/include", "include" }
+      files { "include/*.h", "src/**.h", "src/**.c" }
+ 
+      configuration "Debug"
+         defines { "DEBUG" }
+         flags { "Symbols" }
+ 
+      configuration "Release"
+         defines { "NDEBUG" }
+         flags { "Optimize" }     
